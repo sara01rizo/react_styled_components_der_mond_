978ccbd7c1d8040796f5202c54f3828c81090efa
sara pulido <sara01rizo@gmail.com>
2023-02-01 12:31:58 +0100

    Create simple styled button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import styled from 'styled-components'
 import './App.css';
+
+const StyledButton = styled.button`
+  cursor: pointer;
+  border: 2px solid #4caf50;
+  background-color: #4caf50;
+  color: white;
+  border-radius: 2.5px;
+  padding: 15px 32px;
+  font-size: 16px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  color: white;
+  transition: 0.5s all ease-out;
+`
 
 function App() {
   return (
     <div className="App">
-      
+      {/* <button>Button</button> */}
+      <StyledButton>Styled Button</StyledButton>
     </div>
   );
 }
