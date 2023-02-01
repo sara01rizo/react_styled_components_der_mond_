07afd5241a66191591c7825c1157d5b1ae69cea9
sara pulido <sara01rizo@gmail.com>
2023-02-01 12:47:32 +0100

    Task: separation of concenrs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
-import styled from 'styled-components'
+import StyledButton from './components/Button/Button'
 import './App.css';
 
-const StyledButton = styled.button`
-  cursor: pointer;
-  border: 2px solid #4caf50;
-  background-color: #4caf50;
-  color: white;
-  border-radius: 2.5px;
-  padding: 15px 32px;
-  font-size: 16px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  color: white;
-  transition: 0.5s all ease-out;
-`
 
 function App() {
   return (
